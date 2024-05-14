6323162921f1c021344d9a010d674a2ef5abe449
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2024-05-14 23:20:30 +0530

    Minor changes in Chip
    -> To provide more functionality

--- src/components/Chip.jsx
+++ src/components/Chip.jsx
@@ -2,16 +2,33 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { colors } from '../constants/theme'
 
-const Chip = ({ text, onPress, isSelected }) => {
+const Chip = ({ 
+  text, onPress, isSelected,
+  backgroundColor, textColor, fontSize, bold
+}) => {
   return (
     <TouchableOpacity
       activeOpacity={0.8}
       onPress={onPress}
       style={{
-        backgroundColor: isSelected ? colors.green : colors.lightgray, paddingVertical: 8, paddingHorizontal: 12, borderRadius: 16
+        backgroundColor: 
+          !backgroundColor ?
+            isSelected ? colors.green : colors.lightgray
+          : backgroundColor, 
+        paddingVertical: 8, paddingHorizontal: 12, 
+        borderRadius: 16
       }}
     >
-      <Text style={{ fontSize: 14, includeFontPadding: false, color: isSelected ? colors.white : colors.black }}>{text}</Text>
+      <Text 
+        style={{ 
+          color: 
+            !textColor 
+              ? isSelected ? colors.white : colors.black 
+            : textColor,
+          fontSize: !fontSize ? 14 : fontSize, 
+          includeFontPadding: false,
+          fontWeight: bold && '600'
+        }}>{text}</Text>
     </TouchableOpacity>
   )
 }
