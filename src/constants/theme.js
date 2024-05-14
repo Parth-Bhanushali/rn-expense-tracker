39db43544a612c3debb601ccbdd9f19c196eaedd
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2024-05-14 17:29:03 +0530

    Create theme.js and declare the colors in use

---
+++ src/constants/theme.js
@@ -0,0 +1,12 @@
+export const colors = {
+    orange: "#EC7017",
+    black: "black",
+    white: "white",
+    red: "#D93D3D",
+    green: "#449B26",
+    textNormal: "#353535",
+    lightgray: "#EBEBEB",
+    darkgray: "#616C76",
+    blue: "#5CAFF654",
+    textInputBackground: "F2F2F2"
+}
