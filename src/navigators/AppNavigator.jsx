3ac99e01abe4d4a0c86d1e8f12f5f2338f74d40a
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2024-05-14 15:39:42 +0530

    Create AppNavigator.jsx for managing root navigation

---
+++ src/navigators/AppNavigator.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+
+const AppNavigator = () => {
+  const Stack = createStackNavigator()
+  return (
+    <NavigationContainer>
+
+    </NavigationContainer>
+  )
+}
+
+export default AppNavigator
