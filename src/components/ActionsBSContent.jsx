8eb5e98e957e0c7b428ec2424e02f77ff59b9d87
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2024-05-15 19:08:18 +0530

    Prevent using actions on approved or rejected requests
    -> Changes allowed only on pending requests

--- src/components/ActionsBSContent.jsx
+++ src/components/ActionsBSContent.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 import editIcon from '../assets/edit.png'
 import deleteIcon from '../assets/delete.png'
 import { colors } from '../constants/theme'
+import { Status } from '../constants/dummy'
 
 const ActionsBSContent = ({ item, index, bSContentMeasuredHeight, setBSContentMeasuredHeight, onOptionSelected }) => {
+  const status = item?.status
+
   const OPTIONS = {
     edit: "EDIT",
     delete: "DELETE"
@@ -25,7 +28,7 @@ const ActionsBSContent = ({ item, index, bSContentMeasuredHeight, setBSContentMe
       }}
     >
 
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingTop: 8, paddingBottom: 4, paddingHorizontal: 16, gap: 8, flex: 1 }}>
+      <View style={{ flexDirection: 'row', display: status == Status.PENDING ? 'flex' : 'none', justifyContent: 'space-between', paddingTop: 8, paddingBottom: 4, paddingHorizontal: 16, gap: 8, flex: 1 }}>
         <View style={{ justifyContent: 'center' }}>
           <TouchableOpacity
             onPress={() => onOptionSelected(OPTIONS.edit, item, index)}
@@ -53,6 +56,12 @@ const ActionsBSContent = ({ item, index, bSContentMeasuredHeight, setBSContentMe
             />
           </TouchableOpacity>
         </View>
+      </View>
+
+      <View style={{ flexDirection: 'row', display: status != Status.PENDING ? 'flex' : 'none', justifyContent: 'space-between', paddingTop: 8, paddingBottom: 4, paddingHorizontal: 16, gap: 8, flex: 1 }}>
+        <Text style={{ flex: 1, textAlign: 'center', fontSize: 16, includeFontPadding: false }}>Sorry, the status is already: 
+          <Text style={{ color: status == Status.APPROVED ? colors.green : status == Status.REJECTED && colors.red, fontWeight: '600' }}>  {status}</Text>
+        </Text>
       </View>
     </View>
   )
